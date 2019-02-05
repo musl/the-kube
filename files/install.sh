b597f98b1b2f674a88a9ee6813dd3fcb4c270258
Mike Hix <mhix@newrelic.com>
2019-02-04 22:32:21 -0800

    Use /tmp for coreos install working directory

--- files/install.sh
+++ files/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
 
+cd /tmp
+
 curl -O http://zero.kube.net:8081/coreos_production_image.bin.bz2
 coreos-install -d /dev/mmcblk0 -f /usr/share/oem/image.tar.gz -i /usr/share/oem/install.ign -f coreos_production_image.bin.bz2
 
